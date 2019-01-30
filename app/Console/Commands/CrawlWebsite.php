f650732608ed339d5bf7d1a7519db837fccc9aaa
Arnold Labastilla <arnold@plxrmedia.com>
2019-01-30 23:02:44 +0800

    add web crawler using spatie/crawler

---
+++ app/Console/Commands/CrawlWebsite.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Crawler\Crawler;
+use App\CrawlLogger;
+
+class CrawlWebsite extends Command
+{
+    /** @var \Spatie\Crawler\Crawler */
+    protected $crawler;
+
+    /** @var \App\CrawlLogger */
+    protected $crawlLogger;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'crawl:start {url} {--output=log.txt}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command that crawls a website.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(CrawlLogger $crawlLogger)
+    {
+        parent::__construct();
+
+        $this->crawler = Crawler::create();
+
+        $this->crawlLogger = $crawlLogger;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $url = $this->argument('url');
+        $output = $this->option('output');
+
+        $this->line("Start crawling {$url}...");
+
+        $this->crawlLogger->setOutput($this->getOutput());
+
+        if ($output && file_exists($output)) {
+            if (! $this->confirm("The output file `{$output}` already exists. Overwrite it? (y/n)")) {
+                exit('Aborting...');
+            }
+
+            $this->crawlLogger->setOutputFile($output);
+        }
+
+        $this->crawler
+            ->setCrawlObserver($this->crawlLogger)
+            ->startCrawling($url);
+    }
+}
---
+++ app/CrawlLogger.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace App;
+
+use Spatie\Crawler\CrawlObserver;
+use Psr\Http\Message\UriInterface;
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Exception\RequestException;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CrawlLogger extends CrawlObserver
+{
+    const UNRESPONSIVE_HOST = 'Host did not respond';
+
+    /**
+     * @var \Symfony\Component\Console\Output\OutputInterface
+     */
+    protected $consoleOutput;
+
+    /**
+     * @var array
+     */
+    protected $crawledUrls = [];
+
+    /**
+     * @var string|null
+     */
+    protected $outputFile = null;
+
+    /**
+     * Called when the crawl will crawl the url.
+     *
+     * @param \Psr\Http\Message\UriInterface $url
+     */
+    public function willCrawl(UriInterface $url)
+    {
+    }
+
+    /**
+     * Called when the crawl has ended.
+     */
+    public function finishedCrawling()
+    {
+        $this->consoleOutput->writeln('');
+        $this->consoleOutput->writeln('Crawling summary');
+        $this->consoleOutput->writeln('----------------');
+        
+        ksort($this->crawledUrls);
+        
+        foreach ($this->crawledUrls as $statusCode => $urls) {
+            $colorTag = $this->getColorTagForStatusCode($statusCode);
+            $count = count($urls);
+            
+            if (is_numeric($statusCode)) {
+                $this->consoleOutput->writeln("<{$colorTag}>Crawled {$count} url(s) with statuscode {$statusCode}</{$colorTag}>");
+            }
+            
+            if ($statusCode == static::UNRESPONSIVE_HOST) {
+                $this->consoleOutput->writeln("<{$colorTag}>{$count} url(s) did have unresponsive host(s)</{$colorTag}>");
+            }
+        }
+
+        $this->consoleOutput->writeln('');
+    }
+
+    protected function getColorTagForStatusCode(string $code): string
+    {
+        if ($this->startsWith($code, '2')) {
+            return 'info';
+        }
+        if ($this->startsWith($code, '3')) {
+            return 'comment';
+        }
+        return 'error';
+    }
+
+    /**
+     * @param string|null $haystack
+     * @param string|array $needles
+     *
+     * @return bool
+     */
+    public function startsWith($haystack, $needles): bool
+    {
+        foreach ((array) $needles as $needle) {
+            if ($needle != '' && substr($haystack, 0, strlen($needle)) === (string) $needle) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function crawled(
+        UriInterface $url,
+        ResponseInterface $response,
+        ?UriInterface $foundOnUrl = null
+    ) {
+        $statusCode = $response->getStatusCode();
+
+        $reason = $response->getReasonPhrase();
+        
+        $colorTag = $this->getColorTagForStatusCode($statusCode);
+        
+        $timestamp = date('Y-m-d H:i:s');
+        
+        $message = "{$statusCode} {$reason} - ".(string) $url;
+
+        if ($this->outputFile && $colorTag === 'error') {
+            file_put_contents($this->outputFile, $message.PHP_EOL, FILE_APPEND);
+        }
+        
+        $this->consoleOutput->writeln("<{$colorTag}>[{$timestamp}] {$message}</{$colorTag}>");
+        
+        $this->crawledUrls[$statusCode][] = $url;
+    }
+
+    public function crawlFailed(
+        UriInterface $url,
+        RequestException $requestException,
+        ?UriInterface $foundOnUrl = null
+    ) {
+        $statusCode = self::UNRESPONSIVE_HOST;
+        
+        $reason = $requestException->getResponse()
+            ? $requestException->getResponse()->getReasonPhrase()
+            : $requestException->getMessage();
+        
+        $colorTag = $this->getColorTagForStatusCode($statusCode);
+        
+        $timestamp = date('Y-m-d H:i:s');
+        
+        $message = "{$statusCode}: {$reason} - ".(string) $url;
+        
+        if ($foundOnUrl) {
+            $message .= " (found on {$foundOnUrl})";
+        }
+
+        if ($this->outputFile) {
+            file_put_contents($this->outputFile, $message.PHP_EOL, FILE_APPEND);
+        }
+        
+        $this->consoleOutput->writeln("<{$colorTag}>[{$timestamp}] {$message}</{$colorTag}>");
+        
+        $this->crawledUrls[$statusCode][] = $url;
+    }
+
+    /**
+     * Set the output interface.
+     * 
+     * @param \Symfony\Component\Console\Output\OutputInterface $consoleOutput
+     */
+    public function setOutput(OutputInterface $consoleOutput)
+    {
+        $this->consoleOutput = $consoleOutput;
+    }
+
+    /**
+     * Set the filename to write the output log.
+     *
+     * @param string $filename
+     */
+    public function setOutputFile($filename)
+    {
+        $this->outputFile = $filename;
+    }
+}
